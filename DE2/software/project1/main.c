98d14de8603e42c9d06ff590de9ee5443df40767
Alex Charles <alexliam.charles@gmail.com>
2016-03-15 17:03:25 -0700

    initial commit of de2 code - all from proj1. doesnt compile

---
+++ DE2/software/project1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "menu.h"
+#include "graphics.h"
+#include "touchscreen.h"
+#include "control.h"
+#include "load_node.h"
+#include "search.h"
+#include "gps.h"
+
+int main(void) {
+	init_control();
+	load_from_sd();
+	s_listen();
+	printf("\nDONE\n");
+	return 0;
+}
---
+++ DE2/software/project1/tests/tests.h
@@ -0,0 +1,6 @@
+#ifndef TESTS_H_
+#define TESTS_H_
+
+int hashmap_test(void);
+
+#endif /* TESTS_H_ */
