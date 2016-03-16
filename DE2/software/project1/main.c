80e5e90332fccf643bc97256dbe95f3aeaed5ed6
Kwando <kwando9874123@hotmail.com>
2016-03-16 14:52:22 -0700

    i dun care

--- DE2/software/project1/main.c
+++ DE2/software/project1/main.c
@@ -2,9 +2,19 @@
 #include "graphics.h"
 #include "touchscreen.h"
 #include "gps.h"
+#include "bluetooth.h"
 
 int main(void) {
-
+	printf("Starting...\n");
+	Init_Bluetooth();
+	dataMode();
+	printf("Connection successful!\n");
+	while(1){
+		char c = getCharBluetooth();
+		if (c){
+			printf("%c", c);
+		}
+	}
 	printf("\nDONE\n");
 	return 0;
 }
