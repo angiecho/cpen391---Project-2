3cde363e45089a60c133553d04d94065c96fd55f
William Tang <williamhientang@gmail.com>
2016-03-16 17:11:24 -0700

    forgot id; need to convert sender/receiver bits to an id

--- DE2/software/project1/contact/contact.c
+++ DE2/software/project1/contact/contact.c
@@ -4,10 +4,11 @@
 const char* contact_names[] = {"CHARLES", "CHO", "CALEB"};
 const char* passwords[] = {"0001", "0002", "0003"};
 
-contact init_contact(char* name, char* pw){
+contact init_contact(char* name, char* pw, int id){
     contact c;
     c.name = name;
     c.pw = pw;
+    c.id = id;
     return c;
 }
 
@@ -15,7 +16,7 @@ void init_contacts(){
 	contacts = malloc(sizeof(contact)*N_CONTACTS);
 
 	for(int i = 0; i < N_CONTACTS; i++){
-		contacts[i] = init_contact(contact_names[i], passwords[i]);
+		contacts[i] = init_contact(contact_names[i], passwords[i], i);
 	}
 }
 
