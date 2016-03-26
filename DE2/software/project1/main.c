c8f3424f2f0f9865d1cb795266a9f43e49f7e933
William Tang <williamhientang@gmail.com>
2016-03-26 16:39:12 -0700

    need data from bluetooth/touchscreen

--- DE2/software/project1/main.c
+++ DE2/software/project1/main.c
@@ -4,25 +4,22 @@
 #include "aes_encryption.h"
 
 int main(void) {
-	/*
-    	//TODO: need to get key and iv from android
-    	char* text = "01110TESTING TESTING\0\0\0";
-        char* key = "1234567890qwerty";
-        char* IV = "uiopasdfghjklzxc";
-        char* buffer;
+  	//TODO: need to get key and iv from android
+    char* text = "TESTINGWITH16BTS";
+    char* key = "1234567890qwerty";
+    char* IV = "uiopasdfghjklzxc";
+    char* buffer;
 
-        buffer = (char*)calloc(1, BUFFER_SIZE);
-        strncpy(buffer, message, BUFFER_SIZE);
+    buffer = (char*)calloc(1, BUFFER_SIZE);
+    strncpy(buffer, message, BUFFER_SIZE);
 
-        printf("Encryption Test Start\n");
-        printf("text: %s\n", text);
-        encrypt(buffer, BUFFER_SIZE, IV, key, KEY_SIZE);
-        printf("encrypted cipher: %s\n", buffer); //print_cipher(buffer, BUFFER_SIZE);
-        decrypt(buffer, KEY_SIZE, IV, key, KEY_SIZE);
-        printf("decrypted message: %s\n", buffer);
+    printf("Encryption Test Start\n");
+    printf("text: %s\n", text);
+    encrypt(buffer, BUFFER_SIZE, IV, key, KEY_SIZE);
+    print_cipher(buffer, BUFFER_SIZE);
+    decrypt(buffer, KEY_SIZE, IV, key, KEY_SIZE);
+    printf("decrypted message: %s\n", buffer);
 
-    	return 0;
-    	*/
-	}
 	printf("\nDONE\n");
+	return 0;
 }
--- android/MessagingApp/app/src/main/java/team22/messagingapp/AESEncryption.java
+++ android/MessagingApp/app/src/main/java/team22/messagingapp/AESEncryption.java
@@ -1,50 +1,67 @@
+package team22.messagingapp;
+
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 
-public class AESEncryption {
-    static String text = "01110TESTING TESTING\0\0\0";
-    static String key = "1234567890qwerty";
-    static String IV = "uiopasdfghjklzxc";
+class AESEncryption {
+    static String text = "TESTINGWITH16BTS"; // 16 byte blocks
+    static String key; // 16 bytes
+    static String iv; // 16 bytes
 
-    public byte[] encrypt(String text, String key) throws Exception {
+    public static byte[] encrypt(String text) throws Exception {
+        SecretKeySpec keyspec = new SecretKeySpec(key.getBytes("US-ASCII"), "AES");
+        IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes("US-ASCII"));
+
         Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("US-ASCII"), "AES");
-        IvParameterSpec iv = new IvParameterSpec(IV.getBytes("US-ASCII"));
-
-        cipher.init(Cipher.ENCRYPT_MODE, secret_key, iv);
+        cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
         return cipher.doFinal(text.getBytes("US-ASCII"));
     }
 
-    public String decrypt(byte[] text, String key) throws Exception{
-        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("US-ASCII"), "AES");
-        IvParameterSpec iv = new IvParameterSpec(IV.getBytes("US-ASCII"));
+    public static String decrypt(byte[] text) throws Exception{
+        SecretKeySpec keyspec = new SecretKeySpec(key.getBytes("US-ASCII"), "AES");
+        IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes("US-ASCII"));
 
-        cipher.init(Cipher.DECRYPT_MODE, secret_key, iv);
+        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
         return new String(cipher.doFinal(text), "US-ASCII");
     }
 
-    public print_cipher(byte[] cipher ,int length){
-        for(int i = 0; i < lenght; i++){
+    /*
+        Print the encrypted cipher.
+     */
+    public static void print_cipher(byte[] cipher ,int length){
+        for(int i = 0; i < length; i++){
             System.out.print(new Integer(cipher[i]));
         }
         System.out.println("");
     }
 
-    public void main(String[] args) {
+    /*
+       Set key and IV based on gps/touchscreen data and do something with the touchscreen while we're at it.
+    */
+    public static String get_gps() throws Exception{
+        return "1234567890qwerty";
+    }
+
+    public static String get_ts() throws Exception{
+        return "uiopasdfghjklzxc";
+    }
+
+    public static void main(String[] args) {
         try {
             System.out.println("Encryption Test Start\\n");
+            key = get_gps();
+            iv = get_ts();
+
             System.out.println("text: " + text);
 
-            byte[] cipher = encrypt(header, key);
+            byte[] cipher = encrypt(text);
 
             System.out.print("cipher:  ");
             print_cipher(cipher, cipher.length);
 
-            String decryption = decrypt(cipher, key);
+            String decryption = decrypt(cipher);
             System.out.println("decryption: " + decryption);
 
         } catch(Exception e) {
