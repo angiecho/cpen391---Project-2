3dfa0ab25cc76ea6ac1f63f636aed8e10db8e015
William Tang <williamhientang@gmail.com>
2016-03-29 20:04:27 -0700

    hold up

--- android/MessagingApp/app/src/main/java/team22/messagingapp/Contacts.java
+++ android/MessagingApp/app/src/main/java/team22/messagingapp/Contacts.java
@@ -17,7 +17,6 @@ public class Contacts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
         checkUser();
-
     }
 
     public void checkUser(){
