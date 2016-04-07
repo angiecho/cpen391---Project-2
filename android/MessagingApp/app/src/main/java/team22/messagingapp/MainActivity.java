f4830dc376c46bab66a3e6f5ab117ce630ed6bc3
Alex Charles <Alex Charles>
2016-04-06 21:25:45 -0700

    added fuzzy search - may require some tuning

--- android/MessagingApp/app/src/main/java/team22/messagingapp/MainActivity.java
+++ android/MessagingApp/app/src/main/java/team22/messagingapp/MainActivity.java
@@ -130,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
             if (s.length() >= 3){
                 String parameter = s.toString();
                 ArrayList<Message> messages = getAllMessages();
-                ids = findFuzzyMatches(messages, parameter);
+                ids = findFuzzyMatches(messages, parameter, 0.7);
                 findMatchMessages(ids);
             }
             else {
@@ -466,12 +466,63 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //ToDo: Implement this!
-    private ArrayList<Integer> findFuzzyMatches(ArrayList<Message> messages, String parameter){
+    private ArrayList<Integer> findFuzzyMatches(ArrayList<Message> messages,
+                                                String parameter, double fuzzyness){
         ArrayList<Integer> matchingIds = new ArrayList<Integer>();
-        matchingIds.add(1);
+
+        for (Message message : messages) {
+            int dist = getLevenshteinDistance(message.text, parameter);
+            int length = Math.max(message.text.length(), parameter.length());
+            double score = 1.0 - (double)dist / length;
+
+            if (score > fuzzyness) {
+                matchingIds.add(message.id);
+            }
+        }
 
         return matchingIds;
+    }
+
+    private int getLevenshteinDistance(String lhs, String rhs) {
+        int len0 = lhs.length() + 1;
+        int len1 = rhs.length() + 1;
+
+        // the array of distances
+        int[] cost = new int[len0];
+        int[] newcost = new int[len0];
+
+        // initial cost of skipping prefix in String s0
+        for (int i = 0; i < len0; i++) cost[i] = i;
+
+        // dynamically computing the array of distances
+
+        // transformation cost for each letter in s1
+        for (int j = 1; j < len1; j++) {
+            // initial cost of skipping prefix in String s1
+            newcost[0] = j;
+
+            // transformation cost for each letter in s0
+            for(int i = 1; i < len0; i++) {
+                // matching current letters in both strings
+                int match = (lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1;
+
+                // computing cost for each transformation
+                int cost_replace = cost[i - 1] + match;
+                int cost_insert  = cost[i] + 1;
+                int cost_delete  = newcost[i - 1] + 1;
+
+                // keep minimum cost
+                newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
+            }
+
+            // swap cost/newcost arrays
+            int[] swap = cost;
+            cost = newcost;
+            newcost = swap;
+        }
+
+        // the distance is the cost for transforming all letters in both strings
+        return cost[len0 - 1];
     }
 
     private ArrayList<Message> getAllMessages(){
